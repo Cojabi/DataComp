db1a2205e0f33d2e77bfde7f0a5f8e85f35f01f4
Colin Birkenbihl <colin.birken@gmail.com>
2018-06-29 10:42:35 +0200

    made functions callable

--- DataComp/__init__.py
+++ DataComp/__init__.py
@@ -1,1 +1,5 @@
+from .stats import p_correction, test_categorical, test_num_dist
+from .boxplots import all_features_bp, single_feature_bp, plot_sig_feats
+from .data_functions import get_data, create_zipper, qc_prop_matching
+
 name = "DataComp"
