9c2498091d870559ba5a58eeb28b734ea69151af
Colin Birkenbihl <colin.birken@gmail.com>
2018-07-12 23:21:47 +0200

    added longitudinal progression table functions

--- DataComp/longitudinal.py
+++ DataComp/longitudinal.py
@@ -39,3 +39,64 @@ def transform_to_longitudinal(df, feats, pat_col, time_col, save_folder):
         # save longitudinal version of the current feature
         save_path = os.path.join(save_folder, feat + "_longitudinal.csv")
         long_df.to_csv(save_path)
+
+##### ONGOING WORK######
+
+def calc_prog_scores(values, bl_index, method):
+    """ """
+
+    def _z_score_formula(x, bl, sd):
+        """ """
+        return (x - bl) / sd
+
+    def _pobl_formula(x, bl):
+        """ """
+        return x / bl
+
+    # get baseline value
+    bl = values.loc[bl_index]
+
+    # when there is no baseline measurement present return NaN for all values
+    if not pd.isnull(bl):
+        if method == "z-score":
+            # calculate standard deviation
+            sd = np.std(values)
+            return values.apply(_z_score_formula, args=(bl, sd))
+        elif method == "pobl":
+            return values.apply(_pobl_formula, args=(bl,))
+    else:
+        return np.nan
+
+
+def create_progression_tables(dfs, feats, time_col, patient_col, method, bl_index):
+    """ """
+
+    for df in dfs:
+        patients = df[patient_col]
+        # create dataframe copy to keep from alternating original dataframe
+        prog_df = df[feats][::]
+
+        for feat in feats:
+
+            for patient in patients.unique():
+                # collect values for sinlge patient
+                pat_inds = df[df[patient_col] == patient].index
+                # create value series storing the values of a patient
+                values = df.loc[pat_inds, feat]
+                values.index = df.loc[pat_inds, time_col]
+
+                # calculate scores for patient and reindex to merge back into dataframe copy
+                try:  # DEBUGGING MESS
+                    try:
+                        scores = calc_prog_scores(values, bl_index, method)
+                        try:
+                            scores.index = pat_inds
+                            prog_df.loc[pat_inds, feat] = scores
+                        except AttributeError:
+                            prog_df.loc[pat_inds, feat] = scores
+                    except ValueError:
+                        print(pat_inds)
+                except KeyError:
+                    print(pat_inds)
+
+    return prog_df
