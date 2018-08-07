eb65295beab7eedaec08ef883ec18e002d2200d8
Colin Birkenbihl <colin.birken@gmail.com>
2018-08-07 15:37:26 +0200

    updated documentation in longitudinal

--- datacomp/longitudinal.py
+++ datacomp/longitudinal.py
@@ -3,63 +3,37 @@ import os
 import numpy as np
 
 
-# !!! STILL VERY TIME INEFFICIENT. WORKS FOR NOW BUT NEEDS REWORK LATER ON !!!
-def transform_to_longitudinal(df, feats, pat_col, time_col, save_folder):
-    """
-    Transforms a long format (each visit of patient stored in one row) dataframe feature into a longitudinal format
-    dataframe. The values in time column will give the column names while one row will store all the consecutive
-    visits of a patient.
-
-    :param df: The pandas dataframe storing the features in long format
-    :param feats: A list of features for which longitudinal dataframes shall be constructed
-    :param pat_col: The column name listing the patient IDs
-    :param time_col: The column name listing the time events (visits, months...)
-    :param save_folder: A folder in which the longitudinal dataframes shall be saved.
-    :return:
-    """
-    # create dataframe in which longitudinal data is stored
-    long_df = pd.DataFrame()
-    patients = df[pat_col]
-
-    for feat in feats:
-
-        for patient in patients.unique():
-            # collect data relevant for patient
-            pat_df = df[df[pat_col] == patient][::]
-            # include patient data as row into dataframe
-            pat_df.index = pat_df[time_col]
-            long_df = long_df.append(pat_df[feat])
-
-        # set labels for longitudinal dataframe
-        long_df.index = patients.unique()
-        # try to convert column names into integers for appropriate sorting
-        try:
-            long_df.columns = long_df.columns.astype(int)
-        except TypeError:
-            pass
-        # save longitudinal version of the current feature
-        save_path = os.path.join(save_folder, feat + "_longitudinal.csv")
-        long_df.to_csv(save_path)
-
-
 def calc_prog_scores(time_series, bl_index, method):
     """
     Calculates the progression scores. Can be done using either a z-score normalization to baseline or expressing the
-    score as percent of baseline value.
+    score as ratio of baseline value.
 
     :param time_series: pandas.Series storing the values at the different points in time which shall be transformed into
     progression scores.
     :param bl_index: Index of time_series where the baseline value is stored.
-    :param method: Specifies which progression score should be calculated. z-score or percent of baseline
+    :param method: Specifies which progression score should be calculated. z-score or ratio of baseline
     :return:
     """
 
     def _z_score_formula(x, bl, sd):
-        """ """
+        """
+        Calculates a z-score.
+
+        :param x: Feature value
+        :param bl: Baseline feature value
+        :param sd: Standard deviation
+        :return: z-score
+        """
         return (x - bl) / sd
 
-    def _pobl_formula(x, bl):
-        """ """
+    def _robl_formula(x, bl):
+        """
+        Calculates the ratio between the current feature value and the baseline feature value.
+
+        :param x: Feature Value
+        :param bl: Baseline feature Value
+        :return: Baseline feature value ratio
+        """
         return x / bl
 
     # get baseline value
@@ -75,8 +49,8 @@ def calc_prog_scores(time_series, bl_index, method):
             sd = np.std(time_series)
             return time_series.apply(_z_score_formula, args=(bl_value, sd))
 
-        elif method == "pobl":
-            return time_series.apply(_pobl_formula, args=(bl_value,))
+        elif method == "robl":
+            return time_series.apply(_robl_formula, args=(bl_value,))
 
     else:
         return np.nan
@@ -166,3 +140,41 @@ def analyze_longitudinal_feats(dfs, time_col, bl_index, cat_feats=None, num_feat
                                                       exclude=exclude, include=include, verbose=False)
     return p_values, red_df_store
 """
+
+# !!! STILL VERY TIME INEFFICIENT. WORKS FOR NOW BUT NEEDS REWORK LATER ON !!!
+def transform_to_longitudinal(df, feats, pat_col, time_col, save_folder):
+    """
+    Transforms a long format (each visit of patient stored in one row) dataframe feature into a longitudinal format
+    dataframe. The values in time column will give the column names while one row will store all the consecutive
+    visits of a patient.
+
+    :param df: The pandas dataframe storing the features in long format
+    :param feats: A list of features for which longitudinal dataframes shall be constructed
+    :param pat_col: The column name listing the patient IDs
+    :param time_col: The column name listing the time events (visits, months...)
+    :param save_folder: A folder in which the longitudinal dataframes shall be saved.
+    :return:
+    """
+    # create dataframe in which longitudinal data is stored
+    long_df = pd.DataFrame()
+    patients = df[pat_col]
+
+    for feat in feats:
+
+        for patient in patients.unique():
+            # collect data relevant for patient
+            pat_df = df[df[pat_col] == patient][::]
+            # include patient data as row into dataframe
+            pat_df.index = pat_df[time_col]
+            long_df = long_df.append(pat_df[feat])
+
+        # set labels for longitudinal dataframe
+        long_df.index = patients.unique()
+        # try to convert column names into integers for appropriate sorting
+        try:
+            long_df.columns = long_df.columns.astype(int)
+        except TypeError:
+            pass
+        # save longitudinal version of the current feature
+        save_path = os.path.join(save_folder, feat + "_longitudinal.csv")
+        long_df.to_csv(save_path)
