8a56e4881dfd2751eb6e13510a0ebcda8c0864a6
Colin Birkenbihl <colin.birken@gmail.com>
2018-08-16 00:06:46 +0200

    adapted create prop matching dfs

--- datacomp/prop_matching.py
+++ datacomp/prop_matching.py
@@ -1,13 +1,15 @@
 import pandas as pd
 
+from .datacollection import DataCollection
 
-def create_prop_matched_dfs(matches_path, dfs):
+
+def create_prop_matched_dfs(matches_path, datacol):
     """
     Creats a new list of dataframes but now only containing the matched cases. Propensity Score Matching must be performed
     previously.
     :param matches_path: Path to a csv which contains the matched data. 2 Columns: one lists the subjects of df1 and
     the other lists the matching sample from df2.
-    :param dfs: list of dataframes
+    :param datacol: list of dataframes
     :return: list of dataframes containing only the matches samples
     """
 
@@ -17,9 +19,9 @@ def create_prop_matched_dfs(matches_path, dfs):
 
     # create dfs containing only matched data. Try to get oder of dataframes and matching columns correct
     try:
-        prop_dfs = [dfs[1].loc[matched.index], dfs[0].loc[matched["Match"]]]
+        prop_dfs = [datacol[1].loc[matched.index], datacol[0].loc[matched["Match"]]]
     except KeyError:
-        prop_dfs = [dfs[0].loc[matched.index], dfs[1].loc[matched["Match"]]]
+        prop_dfs = [datacol[0].loc[matched.index], datacol[1].loc[matched["Match"]]]
 
-    return prop_dfs
+    return DataCollection(prop_dfs, datacol.df_names)
 
