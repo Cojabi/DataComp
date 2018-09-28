50766b8be6911dc564cc5d0184dab231418b66dd
Colin Birkenbihl <colin.birken@gmail.com>
2018-09-28 10:34:38 +0200

    fixed unit tests

--- tests/test_datacollection.py
+++ tests/test_datacollection.py
@@ -11,39 +11,41 @@ test_data_path = os.path.join(dir_path, 'test_data.csv')
 
 df_names = ["Test1", "Test2"]
 
-group_col = "Site"
+group_col = "site"
 
-feat_subset = ["ADAS", "Age", "EmptyFull", "EmptyS1"]
+cat_feats = ["cat1", "cat2", "cat3"]
 
-features = [{"ADAS", "MMSE", "Site", "Age", "Diagnosis",
-             "Class", "EmptyS1", "EmptyFull", "Gender"},
-            {"ADAS", "MMSE", "Site", "Diagnosis", "Age",
-             "Class", "EmptyS1", "EmptyFull", "Gender"}]
+feat_subset = ["feat1", "feat2", "cat1"]
 
-exclude_feats = ["ADAS", "MMSE", "Site", "Diagnosis",
-                 "Age", "Class", "EmptyS1"]
+features = [{"feat1", "feat2", "site", "feat3", "cat1",
+             "cat2", "cat3"},
+            {"feat1", "feat2", "site", "cat1", "feat3",
+             "cat2", "empty1", "cat3"}]
+
+exclude_feats = ["feat1", "feat2", "site", "cat1",
+                 "feat3", "cat2"]
 
 
 class TestDataCollection(unittest.TestCase):
     """ """
 
     def setUp(self):
-        self.datacol = get_data(test_data_path, df_names, group_col)
+        self.datacol = get_data(test_data_path, df_names, cat_feats, group_col)
 
     def test_get_data(self):
         """Test get_data function."""
-        self.assertEqual(self.datacol[0].shape[0], 5)
-        self.assertEqual(self.datacol[1].shape[0], 6)
+        self.assertEqual(self.datacol[0].shape, (5, 7))
+        self.assertEqual(self.datacol[1].shape, (6, 8))
 
     def test_create_zipper(self):
         """Test the create_zipper function."""
         zipper = self.datacol.create_zipper(feat_subset)
 
-        self.assertEqual(zipper["ADAS"],
+        self.assertEqual(zipper["feat2"],
                          ([10, 11, 10, 10], [10, 10, 10, 9]))
 
-        self.assertEqual(zipper["EmptyS1"],
-                         ([], [11.0, 13.0, 15.0, 17.0, 19.0, 21.0]))
+        self.assertEqual(zipper["cat1"],
+                         (["C", "M", "M", "C"], ["A", "A", "C", "A", "A"]))
 
         self.assertEqual(set(zipper.keys()), set(feat_subset))
 
@@ -53,17 +55,17 @@ class TestDataCollection(unittest.TestCase):
 
     def test_get_common_features(self):
         common_feats = self.datacol.get_common_features(exclude=exclude_feats)
-        self.assertEqual(set(common_feats), {"EmptyFull", "Gender"})
+        self.assertEqual(set(common_feats), {"cat3"})
 
     def test_reduce_dfs_to_feat_subset(self):
         reduced_datacol = self.datacol.reduce_dfs_to_feat_subset(feat_subset)
-        self.assertEqual(list(reduced_datacol[0]), feat_subset)
+        self.assertEqual(set.union(*reduced_datacol.get_feature_sets()), set(feat_subset))
 
         # test that changes on reduced df do not affect original df
         changed_feat = feat_subset[0]
         # before change, drop NaNs because in pandas/numpy NaN != NaN
-        self.assertTrue(
-            (reduced_datacol[0][changed_feat].dropna() == self.datacol[0][changed_feat].dropna()).all()
+        self.assertFalse(
+            (reduced_datacol[0][changed_feat].dropna(inplace=True) == self.datacol[0][changed_feat]).all()
         )
         # change
         reduced_datacol[0][changed_feat] = "changed_value"
@@ -73,8 +75,8 @@ class TestDataCollection(unittest.TestCase):
         )
 
     def test_reduce_dfs_to_value(self):
-        reduced_datacol = self.datacol.reduce_dfs_to_value("Gender", 1)
-        self.assertEqual(len(reduced_datacol[0]), 3)
+        reduced_datacol = self.datacol.reduce_dfs_to_value("cat2", 1)
+        self.assertEqual(reduced_datacol[0].shape, (3, 7))
         self.assertEqual(list(reduced_datacol[1].index), ["x23", "x25", "x27"])
 
         # test that changes on reduced df do not affect original df
