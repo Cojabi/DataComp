8942fecb69c794ac8a2250817d4a1ee78aeb4fba
Colin Birkenbihl <colin.birken@gmail.com>
2018-09-28 12:10:33 +0200

    put nested functions into utils

--- datacomp/stats.py
+++ datacomp/stats.py
@@ -7,9 +7,10 @@ import warnings
 from scipy.stats import mannwhitneyu, ttest_ind, chisquare
 from statsmodels.sandbox.stats.multicomp import multipletests
 from statsmodels.multivariate.manova import MANOVA
-from collections import Counter
 
-from .utils import construct_formula
+
+from .utils import construct_formula, _categorical_table, _non_present_values_to_zero, \
+    _test_if_all_vals_equal, _create_result_table, _transform_p_dict
 
 
 def test_num_feats(zipper, feat_subset=None, method=None):
@@ -23,19 +24,6 @@ def test_num_feats(zipper, feat_subset=None, method=None):
     :param method: Specify which statistical test should be used. "u" for Mann-Whitney-U-test and "t" for t-test.
     :return: dictionary storing the p_values of the analysis. Feature names are used as keys.
     """
-
-    def _test_if_all_vals_equal(vals1, vals2):
-        """Checks if the union of two iterables is 1 and returns True if that is the case. Is used in test_num_feats to
-        check if two lists of values have only the same value and nothing else.
-
-        :param vals1:
-        :param vals2:
-        :return:
-        """
-        # build union between values
-        uni = set.union(set(vals1), set(vals2))
-        # if only one value is in the union, they are equal
-        return len(uni) == 1
 
     # if no method is specified used Mann-Whitney-U-test as standard
     if method is None:
@@ -82,7 +70,7 @@ def test_num_feats(zipper, feat_subset=None, method=None):
     return p_values
 
 
-def test_cat_feats(zipper, feat_subset=None, method="chi", print_data=False):
+def test_cat_feats(zipper, feat_subset=None, method=None, print_data=False):
     """
     Performs hypothesis testing to identify significantly deviating categorical features. A chi-squared test is used.
 
@@ -92,41 +80,15 @@ def test_cat_feats(zipper, feat_subset=None, method="chi", print_data=False):
     :return:
     """
 
-    def _categorical_table(data):
-        """
-        Returns the counts of occurences for the categories. Is used to build the observation table
-        for a chi square test.
-
-        :param data:
-        :return:
-        """
-        # count occurences
-        c = Counter(data)
-        # get rid of NaNs
-        c = {key: c[key] for key in c if not pd.isnull(key)}
-        return pd.Series(c)
-
-    def _non_present_values_to_zero(test_data):
-        """
-        Fills keys in the test data of one dataframe if it is not present but present in one of the other datasets.
-
-        :param test_data:
-        :return:
-        """
-        for dataset1 in test_data:
-            for dataset2 in test_data:
-
-                for key in dataset1.keys():
-
-                    if key not in dataset2:
-                        dataset2[key] = 0
-        return test_data
-
     p_values = dict()
 
     # consider all features is no feature subset was specified
     if feat_subset is None:
         feat_subset = zipper.keys()
+
+    # set default method to chi-square test
+    if method is None:
+        method = "chi"
 
     for feat in feat_subset:
         # initiate dict in dict for d1 vs d2, d2 vs d3 etc. per feature
@@ -173,71 +135,6 @@ def p_correction(p_values, counts):
     :return: DataFrame which shows the results of the analysis; p-value, corrected p-value and boolean indicating \
     significance.
     """
-
-    def _transform_p_dict(p_value_dict):
-        """
-        Utility function that transforms a dictionary of dicts into a dataframe representing the dicts as rows
-        (like tuples). Is needed to keep track of the feature names and corresponding values.
-        The underlying datastructures are confusing.
-
-        :param p_value_dict: dictionary of dictionaries storing the p_values
-        :return: dataframe where the keys are added to the p_values as columns
-        """
-        # Turn dictionary of dictionaries into a collection of the key-value pairs represented as nested tuples
-        item_dict = dict()
-
-        for feat in p_value_dict:
-            item_dict[feat] = list(p_value_dict[feat].items())
-
-        # building a matrix (nested lists) by extracting and sorting data from nested tuples
-        # (items[0], (nested_items[0], nested_items[1]))
-        df_matrix = []
-
-        for items in item_dict.items():
-            for nested_items in items[1]:
-                df_matrix.append([nested_items[1], nested_items[0], items[0]])
-        return pd.DataFrame(df_matrix)
-
-    def _create_result_table(result, p_val_col, p_trans, counts):
-        """
-        Builds the dataframe showing the results.
-
-        :param result:
-        :param p_val_col:
-        :param p_trans:
-        :param counts:
-        :return:
-        """
-        # store test results
-        result_table = pd.DataFrame(p_val_col)
-
-        # rename columns and set values in result dataframe
-        result_table.rename(columns={0: "pv"}, inplace=True)
-
-        # insert corrected p_values
-        if result:
-            result_table["cor_pv"] = result[1]
-            result_table["signf"] = result[0]
-        elif result is None:
-            result_table["cor_pv"] = np.nan
-            result_table["signf"] = np.nan
-
-        # combine p_value information with dataset and feature information stored in p_trans
-        result_table = pd.concat([result_table, p_trans], axis=1, join="outer")
-
-        # create multi index brom feature name result_table[2] and datasets result_table[1]
-        result_table.index = result_table[[2, 1]]
-        result_table.index = pd.MultiIndex.from_tuples(result_table.index)
-        result_table.drop([0, 1, 2], axis=1, inplace=True)
-
-        # name index levels
-        result_table.index.levels[0].name = "features"
-        result_table.index.levels[1].name = "datasets"
-
-        # join with counts dataframe to display number of datapoint for each comparison
-        result_table = result_table.join(counts, how="outer")
-
-        return result_table
 
     p_trans = _transform_p_dict(p_values)
 
--- datacomp/utils.py
+++ datacomp/utils.py
@@ -99,3 +99,111 @@ def calc_prog_scores(time_series, bl_index, method):
 
     else:
         return np.nan
+
+def _categorical_table(data):
+    """
+    Returns the counts of occurences for the categories. Is used to build the observation table
+    for a chi square test.
+
+    :param data:
+    :return:
+    """
+    # count occurences
+    c = Counter(data)
+    # get rid of NaNs
+    c = {key: c[key] for key in c if not pd.isnull(key)}
+    return pd.Series(c)
+
+def _non_present_values_to_zero(test_data):
+    """
+    Fills keys in the test data of one dataframe if it is not present but present in one of the other datasets.
+
+    :param test_data:
+    :return:
+    """
+    for dataset1 in test_data:
+        for dataset2 in test_data:
+
+            for key in dataset1.keys():
+
+                if key not in dataset2:
+                    dataset2[key] = 0
+    return test_data
+
+def _test_if_all_vals_equal(vals1, vals2):
+    """Checks if the union of two iterables is 1 and returns True if that is the case. Is used in test_num_feats to
+    check if two lists of values have only the same value and nothing else.
+
+    :param vals1:
+    :param vals2:
+    :return:
+    """
+    # build union between values
+    uni = set.union(set(vals1), set(vals2))
+    # if only one value is in the union, they are equal
+    return len(uni) == 1
+
+def _transform_p_dict(p_value_dict):
+    """
+    Utility function that transforms a dictionary of dicts into a dataframe representing the dicts as rows
+    (like tuples). Is needed to keep track of the feature names and corresponding values.
+    The underlying datastructures are confusing.
+
+    :param p_value_dict: dictionary of dictionaries storing the p_values
+    :return: dataframe where the keys are added to the p_values as columns
+    """
+    # Turn dictionary of dictionaries into a collection of the key-value pairs represented as nested tuples
+    item_dict = dict()
+
+    for feat in p_value_dict:
+        item_dict[feat] = list(p_value_dict[feat].items())
+
+    # building a matrix (nested lists) by extracting and sorting data from nested tuples
+    # (items[0], (nested_items[0], nested_items[1]))
+    df_matrix = []
+
+    for items in item_dict.items():
+        for nested_items in items[1]:
+            df_matrix.append([nested_items[1], nested_items[0], items[0]])
+    return pd.DataFrame(df_matrix)
+
+def _create_result_table(result, p_val_col, p_trans, counts):
+    """
+    Builds the dataframe showing the results.
+
+    :param result:
+    :param p_val_col:
+    :param p_trans:
+    :param counts:
+    :return:
+    """
+    # store test results
+    result_table = pd.DataFrame(p_val_col)
+
+    # rename columns and set values in result dataframe
+    result_table.rename(columns={0: "pv"}, inplace=True)
+
+    # insert corrected p_values
+    if result:
+        result_table["cor_pv"] = result[1]
+        result_table["signf"] = result[0]
+    elif result is None:
+        result_table["cor_pv"] = np.nan
+        result_table["signf"] = np.nan
+
+    # combine p_value information with dataset and feature information stored in p_trans
+    result_table = pd.concat([result_table, p_trans], axis=1, join="outer")
+
+    # create multi index brom feature name result_table[2] and datasets result_table[1]
+    result_table.index = result_table[[2, 1]]
+    result_table.index = pd.MultiIndex.from_tuples(result_table.index)
+    result_table.drop([0, 1, 2], axis=1, inplace=True)
+
+    # name index levels
+    result_table.index.levels[0].name = "features"
+    result_table.index.levels[1].name = "datasets"
+
+    # join with counts dataframe to display number of datapoint for each comparison
+    result_table = result_table.join(counts, how="outer")
+
+    return result_table
