343bf38af11616466c1f8268b26355c02de47876
Colin Birkenbihl <colin.birken@gmail.com>
2019-03-13 16:09:10 +0100

    added bumpversion

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ PACKAGES = setuptools.find_packages(where='src')
 
 setuptools.setup(
     name="datacomp",
-    version='0.0.1',
+    version='0.0.2-dev',
     author="Colin Birkenbihl",
     author_email="colin.birken@gmail.com",
     description="A small example package",
