dc6d2922a75dbfd620cdeda4fb507c6f7aa6ad53
Colin Birkenbihl <colin.birken@gmail.com>
2019-06-05 14:29:58 +0200

    added functions to innit

--- src/datacomp/__init__.py
+++ src/datacomp/__init__.py
@@ -3,6 +3,6 @@
 from .datacollection import DataCollection, get_data, create_datacol
 from .prop_matching import create_prop_matched_dfs, create_prop_matched_dfs_longitudinal
 from .stats import p_correction, test_num_feats, test_cat_feats, manova, calc_conf_inv
-from .utils import get_sig_feats, construct_formula
+from .utils import get_sig_feats, construct_formula, get_diff_feats
 from .visualization import all_feature_boxplots, plot_sig_num_feats, plot_prog_scores, all_feature_kdeplots, \
     plot_entities_per_timepoint, plot_signf_progs, plot_sig_cat_feats, countplot_single_features
